4bd3289834836d7a977cada0706a28e4a7f5dddc
Victoria Apalkova <apalkova_v@hub.sknt.ru>
2020-01-15 15:40:00 +0300

    files

--- app/Http/Controllers/Admin/FileController.php
+++ app/Http/Controllers/Admin/FileController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\Admin\File\Store;
 
 class FileController extends Controller
 {
-    public function store(Request $request)
+    public function store(Store $request)
     {
         $path = $request->file('file')->store('temp');
         return response([
